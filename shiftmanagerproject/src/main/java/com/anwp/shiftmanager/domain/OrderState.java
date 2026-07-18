a84587df219cab3c260f72af0de75936a8fa8926
Gabrielle <gabimaponda.bis@gmail.com>
2026-07-18 18:25:34 +0200

    diagramms

---
+++ shiftmanagerproject/src/main/java/com/anwp/shiftmanager/domain/OrderState.java
@@ -0,0 +1,17 @@
+package com.anwp.shiftmanager.domain;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+*
+* this class defines the states an order can have folowing its live state.
+* **/
+
+public interface OrderState{
+
+// TODO trouver logique qui respecte le schema des cycles de vie d une commande.
+
+
+}
