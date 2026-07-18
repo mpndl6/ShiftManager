102ac394334b5e17582d8284d2d9a20163c7031b
Mzihirat Mzé abdou <mzihiratmzeabdou@MacBook-Air-de-Mzihirat.local>
2026-07-18 16:25:29 +0200

    spring initializer

---
+++ shiftmanagerproject/src/main/java/com/anwp/shiftmanager/ShiftmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.anwp.shiftmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftmanagerApplication.class, args);
+	}
+
+}
---
+++ shiftmanagerproject/src/test/java/com/anwp/shiftmanager/ShiftmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anwp.shiftmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
